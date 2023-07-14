70a049031971c1da41eae3d3cc1d9e213044f4db
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-14 20:07:10 +0900

    特になし

--- priv/mm/gurad.exs
+++ priv/mm/gurad.exs
@@ -1,10 +1,22 @@
 defmodule Guard do
   def what_is(x) when is_number(x) do
-    IO.puts("#{x} is a number")
+    cond do
+      x === 0 ->
+        "#{x} is a number"
+
+      x > 0 ->
+        "#{x} is a number and a positive integer."
+
+      x < 0 ->
+        "#{x} is a number and a negative integer."
+    end
   end
 
   def what_is(x) when is_list(x) do
-    IO.puts("#{inspect(x)} is a list")
+    case x do
+      [] -> IO.puts("#{inspect(x)} is a empty list")
+      _ -> IO.puts("#{inspect(x)} is a list")
+    end
   end
 
   def what_is(x) when is_atom(x) do
--- priv/mm/list.exs
+++ priv/mm/list.exs
@@ -1,4 +1,4 @@
 defmodule Listlen do
   def leng([]), do: 0
-  def leng([_|t]), do: 1 + Enum.count(t)
+  def leng([_ | t]), do: 1 + Enum.count(t)
 end
