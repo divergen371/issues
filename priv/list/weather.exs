fea0e0a2690f6499fc21d98097676e4b8f4f9a4f
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-15 22:14:22 +0900

    フィルタリングに関して、条件にしたい値だけあれば充分

--- priv/list/weather.exs
+++ priv/list/weather.exs
@@ -1,12 +1,12 @@
 defmodule WeatherHistory do
-  def for_location_27([], _target_loc), do: []
+  def for_location([], _target_loc), do: []
 
-  def for_location_27([[time, target_loc, temp, rain] | tail], target_loc) do
-    [[time, target_loc, temp, rain] | for_location_27(tail, target_loc)]
+  def for_location([head = [_, target_loc, _, _] | tail], target_loc) do
+    [head | for_location(tail, target_loc)]
   end
 
-  def for_location_27([_ | tail], target_loc),
-    do: for_location_27(tail, target_loc)
+  def for_location([_ | tail], target_loc),
+    do: for_location(tail, target_loc)
 
   def test_data do
     [
