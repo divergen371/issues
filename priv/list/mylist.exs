0e40a578a23d17b8c45ec1e44dc65e6b06ee7cb8
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-15 22:28:07 +0900

    spanの追加

--- priv/list/mylist.exs
+++ priv/list/mylist.exs
@@ -1,4 +1,12 @@
 defmodule MyList do
   def len([]), do: 0
   def len([_ | tail]), do: 1 + len(tail)
+
+  def span(from, to = from) do
+    []
+  end
+
+  def span(from, to) do
+    [from | span(from + 1, to)]
+  end
 end
