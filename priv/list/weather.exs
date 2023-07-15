a343db5b2df3a0ce88bf6dab1292074a5b6c756f
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-15 22:09:02 +0900

    パターンマッチで表示データを指定する

--- priv/list/weather.exs
+++ priv/list/weather.exs
@@ -1,11 +1,12 @@
 defmodule WeatherHistory do
-  def for_location_27([]), do: []
+  def for_location_27([], _target_loc), do: []
 
-  def for_location_27([[time, 27, temp, rain] | tail]) do
-    [[time, 27, temp, rain] | for_location_27(tail)]
+  def for_location_27([[time, target_loc, temp, rain] | tail], target_loc) do
+    [[time, target_loc, temp, rain] | for_location_27(tail, target_loc)]
   end
 
-  def for_location_27([_ | tail]), do: for_location_27(tail)
+  def for_location_27([_ | tail], target_loc),
+    do: for_location_27(tail, target_loc)
 
   def test_data do
     [
